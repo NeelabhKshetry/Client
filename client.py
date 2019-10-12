55452474f5c8a40ec120a3170230302b4a534284
NeelabhKshetry <48328177+NeelabhKshetry@users.noreply.github.com>
2019-10-11 23:14:22 -0500

    Update client.py

--- client.py
+++ client.py
@@ -11,22 +11,15 @@ hostname = gethostbyaddr(ip)[1]
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((hostname, port))
 
-try:
-    f = open(filename, "utf-8")
-    outputdata = f.read()
-    clientSocket.send(bytes("HTTP/1.1 200 OK", "utf-8"))
+request = "GET /" + filename + "HTTP/1.1\r" 
+clientSocket.send(request)
+full_msg = ""
+while 1:
+    msg = clientSocket.recv(1000)
+    if len(msg) <= 0:
+        break
+    full_msg += msg.decode("utf-8")
 
-    for i in range(0, len(outputdata)):
-        clientSocket.send(bytes(outputdata[i], "utf-8"))
-except IOError:
-    # Send response message for file not found
-    clientSocket.send(bytes("HTTP/1.1 404 Not Found\r\n", "utf-8"))
+print(full_msg)
 
-    errorfile = open("ErrorPage.html", encoding="utf-8")
-    errormessage = errorfile.read()
-
-    for i in range(0, len(errormessage)):
-        clientSocket.send(bytes(errormessage[i], "utf-8"))
-
-    # Close client socket
-    clientSocket.close()
+clientSocket.close()
